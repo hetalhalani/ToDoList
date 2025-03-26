86a3ae77eccfbd0d83d418a0c749f419e0a57e31
hetalhalani <hetal.halani@sjsu.edu>
2025-03-26 11:02:04 -0700

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Hetal Halani on 3/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
